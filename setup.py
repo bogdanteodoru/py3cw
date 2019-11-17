7837262277d304a95f58684bf301310cf445cd58
Bogdan Teodoru <me@bogdanteodoru.com>
2019-11-17 21:19:35 +0200

    Trying to fix Python package upload.

--- setup.py
+++ setup.py
@@ -14,17 +14,7 @@ setup(
     license='MIT',
 
     classifiers=[
-        'Development Status :: 1 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        'Development Status :: 5 - Production/Stable'
     ],
 
     keywords='api 3commas trading crypto bitcoin altcoin',
